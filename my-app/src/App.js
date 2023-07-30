ff0eb19cd13275b148843927e15fdef5eb878990
andreinikolabai <andreiplus2000@gmail.com>
2023-07-30 10:46:23 +0300

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 
-const ContactForm = () => {
+const Formik = () => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -23,42 +23,21 @@ const ContactForm = () => {
     event.preventDefault();
     const { name, email, phone } = formData;
 
-    // Виконуємо валідацію полів
-    let formIsValid = true;
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    if (!name) {
-      setErrors({ ...errors, name: 'Будь ласка, введіть ім\'я' });
-      formIsValid = false;
-    } else {
-      setErrors({ ...errors, name: '' });
-    }
+    const newErrors = {
+      name: !name ? ' Будь ласка, введіть ім\'я' : '',
+      email: !email ? ' Будь ласка, введіть електронну пошту' : !emailRegex.test(email) ? ' Будь ласка, введіть коректну електронну пошту' : '',
+      phone: !phone ? ' Будь ласка, введіть номер телефону' : !/^\d{12}$/.test(phone) ? ' Номер телефону повинен містити до 12 цифр' : '',
+    };
 
-    if (!email) {
-      setErrors({ ...errors, email: 'Будь ласка, введіть електронну пошту' });
-      formIsValid = false;
-    } else if (!emailRegex.test(email)) {
-      setErrors({ ...errors, email: 'Будь ласка, введіть коректну електронну пошту' });
-      formIsValid = false;
-    } else {
-      setErrors({ ...errors, email: '' });
-    }
+    setErrors(newErrors);
 
-    if (!phone) {
-      setErrors({ ...errors, phone: 'Будь ласка, введіть номер телефону' });
-      formIsValid = false;
-    } else if (!/^\d{10,12}$/.test(phone)) {
-      setErrors({ ...errors, phone: 'Номер телефону повинен містити від 10 до 12 цифр' });
-      formIsValid = false;
-    } else {
-      setErrors({ ...errors, phone: '' });
-    }
+    const formIsValid = !Object.values(newErrors).some((error) => error !== '');
 
     if (formIsValid) {
-      // Виконуємо дії після успішної відправки форми
       alert('Дані успішно відправлено: ' + JSON.stringify(formData));
 
-      // Затираємо поля форми
       setFormData({
         name: '',
         email: '',
@@ -78,21 +57,21 @@ const ContactForm = () => {
         <div>
           <label htmlFor="name">Ім'я:</label>
           <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} />
-          {errors.name && <span>{errors.name}</span>}
+          {errors.name && <span className="error-message">{errors.name}</span>}
         </div>
         <div>
           <label htmlFor="email">Електронна пошта:</label>
           <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} />
-          {errors.email && <span>{errors.email}</span>}
+          {errors.email && <span className="error-message">{errors.email}</span>}
         </div>
         <div>
           <label htmlFor="phone">Телефон:</label>
-          <input type="tel" id="phone" name="phone" value={formData.phone} onChange={handleChange} />
-          {errors.phone && <span>{errors.phone}</span>}
+          <input type="phone" id="phone" name="phone" value={formData.phone} onChange={handleChange} />
+          {errors.phone && <span className="error-message">{errors.phone}</span>}
         </div>
         <button type="submit">Відправити</button>
       </form>
   );
 };
 
-export default ContactForm;
+export default Formik;
